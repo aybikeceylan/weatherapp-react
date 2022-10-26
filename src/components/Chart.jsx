4ec5bb1ce4fc2cf327b71ecf4ac0c33ccbb14982
aybike <aybikesayar@gmail.com>
2022-10-27 01:54:13 +0300

    cmmt

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -4,32 +4,28 @@ import Card from 'react-bootstrap/Card';
 const Chart = (weather) => {
 
     console.log(weather);
-    // const { name, weather, main: { temp }, wind: { speed }, main: { humidity } } = weather
-    // console.log(name);
-    // console.log(Object.values(weather)[0].description)
-    // console.log(Object.values(weather)[0].icon)
-    // console.log(temp);
-    // console.log(humidity);
-    // console.log(speed);
+    if (!weather) {
+        return <h1>Loading</h1>
+    }
     return (
 
         <Card style={{ width: '40rem' }} className="mx-auto" >
             <Card.Body>
-                <Card.Title className="mr-0">{weather.name}</Card.Title>
+                <Card.Title className="mr-0">{weather?.name}</Card.Title>
                 <Card.Subtitle className="text-muted my-5">
-                    <h2 className="text-center">{weather.main.temp}°C</h2>
+                    <h2 className="text-center">{weather?.main.temp}°C</h2>
                 </Card.Subtitle>
                 <Card.Text className="text-muted d-flex justify-content-between">
                     <Card.Text>
                         <Card.Text>
-                            {weather.wind.speed} km/h
+                            {weather?.wind.speed} km/h
                         </Card.Text>
                         <Card.Text>
-                            {weather.main.humidity}
+                            {weather?.main.humidity}
                         </Card.Text>
                     </Card.Text>
                     <Card.Text className="text-muted ">
-                        <img className="city-icon" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt={weather.name} />
+                        <img className="city-icon" src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`} alt={weather.name} />
                     </Card.Text>
                 </Card.Text>
             </Card.Body>
